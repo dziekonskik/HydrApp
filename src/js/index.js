0fcacdabf2087bc825e8b8ff7554e9eb182a4b94
Seven <jaseveen@gmail.com>
2020-07-04 12:12:58 +0200

    add js beginning

--- src/js/index.js
+++ src/js/index.js
@@ -6,4 +6,44 @@ registerSW();
 
 /* place your code below */
 
-console.log('HELLO 🚀');
+let glassCounter = document.createElement('div');
+let counter = 0;
+
+const container = document.querySelector('.app__glass');
+const addGlassButton = document.querySelector('.app__increment--js');
+const decrementGlassButton = document.querySelector('.app__decrement--js');
+
+const key = new Date().toISOString().slice(0, 10);
+console.log(key);
+
+function addGlass() {
+  console.log(counter);
+  return counter++;
+}
+
+function reduceGlass() {
+  return counter--;
+}
+
+function handleIncrementCounterButton() {
+  addGlass();
+}
+
+function handledecrementCounterButton() {
+  reduceGlass();
+}
+
+function styleCounter(counter) {
+  glassCounter.innerHTML = counter;
+  glassCounter.classList.add('app__counter');
+}
+
+function appendCounterToContainer() {
+  container.appendChild(glassCounter);
+}
+
+styleCounter();
+appendCounterToContainer();
+
+addGlassButton.addEventListener('click', handleIncrementCounterButton);
+decrementGlassButton.addEventListener('click', handledecrementCounterButton);
